af31109ad3ac6363a764d4bfdc3bb13d7e2f5a85
vishal kumar sharma <136582084+vk0sharma0@users.noreply.github.com>
2024-08-18 22:24:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,14 @@ app.get('/', async (req, res) => {
 
         const response = await axios.get('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY', {
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-                'Accept-Language': 'en-US,en;q=0.9',
-                'Accept-Encoding': 'gzip, deflate, br'
+                
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Referer': 'https://www.nseindia.com',
+    'Connection': 'keep-alive'
+            
+            
             },
             cancelToken: source.token
         });
