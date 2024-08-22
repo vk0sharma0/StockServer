48d43fa55a1c94cc9a3aca03457fee2ae9b7094e
vishal kumar sharma <136582084+vk0sharma0@users.noreply.github.com>
2024-08-22 19:01:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,13 +25,25 @@ async function fetchDataWithRetry(url, retries = 3) {
     try {
         const response = await axiosInstance.get(url, {
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-                'Accept': 'application/json, text/plain, */*',
-                'Referer': 'https://www.nseindia.com/',
-                'Origin': 'https://www.nseindia.com',
-                'Accept-Encoding': 'gzip, deflate, br',
-                'Accept-Language': 'en-US,en;q=0.9',
-                'Content-Type': 'application/json; charset=utf-8',
+                X-Firefox-Spdy: h2
+access-control-allow-headers: Content-Type
+access-control-allow-methods: GET,POST
+access-control-allow-origin: beta.nseindia.com, nseindia.com
+content-encoding: gzip
+content-length: 91313
+content-security-policy: font-src *; default-src 'self' *; img-src * data: https://www.google.co.in https://www.google-analytics.com www.google-analytics.com https://nse-widget.interface.ai; style-src * 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.youtube.com *.gstatic.com https://www.google.com/ *.cloudflare.com *.googletagmanager.com *.highcharts.com *.go-mpulse.net https://stats.g.doubleclick.net/ https://www.google.com/ *.doubleclick.net https://nse-uat-widget.interface.ai/widget-loader.js www.google-analytics.com *.g.doubleclick.net/ https://analytics.google.com/ https://nse-widget.interface.ai
+content-type: application/json; charset=utf-8
+date: Thu, 22 Aug 2024 08:48:13 GMT
+link: <https://www.nseindia.com/assets/webfonts/fa-brands-400.woff2>;rel="preload";as="font";type="font/woff2";crossorigin,<https://www.nseindia.com/assets/webfonts/fa-solid-900.woff2>;rel="preload";as="font";type="font/woff2";crossorigin
+server: Apache
+server-timing: cdn-cache; desc=HIT, edge; dur=27, origin; dur=0, ak_p; desc="1724316493676_399044316_159647767_2744_18309_36_56_41";dur=1
+strict-transport-security: max-age=31536000 ; includeSubDomains ; preload
+vary: Accept-Encoding
+x-content-type-options: nosniff
+x-dns-prefetch-control: off
+x-download-options: noopen
+x-frame-options: SAMEORIGIN
+x-xss-protection: 1; mode=block
             }
         });
         return response.data;
