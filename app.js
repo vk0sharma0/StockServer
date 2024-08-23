08a328559e7fc800c4669406856b1c0b2c44a36e
vishal kumar sharma <136582084+vk0sharma0@users.noreply.github.com>
2024-08-23 14:08:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.get('/', cors(corsOptions), async (req, res) => {
 });
 async function myfun() {
     try {
-        const response = await fetch("https://42630c6c8a6207c013cc7d29f69c7d4f.serveo.net",{headers: {
+        const response = await fetch("https://a31c23d91d3ce671748dba7f0c7f7476.serveo.net",{headers: {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-US,en;q=0.5',
